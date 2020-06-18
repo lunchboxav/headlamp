a72d287218380e34afbbcde4abfac5c5299566df
Joaquim Rocha <joaquim@kinvolk.io>
2020-06-18 20:42:36 +0200

    frontend: Add and use the new StatefulSet class

---
+++ frontend/src/lib/k8s/statefulSet.ts
@@ -0,0 +1,25 @@
+import { apiFactoryWithNamespace } from './apiProxy';
+import { KubeObjectInterface, LabelSelector, makeKubeObject } from './cluster';
+
+export interface KubeStatefulSet extends KubeObjectInterface {
+  spec: {
+    selector: LabelSelector;
+    updateStrategy: {
+      rollingUpdate: {
+        partition: number;
+      };
+      type: string;
+    };
+    [other: string]: any;
+  };
+}
+
+class StatefulSet extends makeKubeObject<KubeStatefulSet>('StatefulSet') {
+  static apiEndpoint = apiFactoryWithNamespace('apps', 'v1', 'statefulsets', true);
+
+  get spec() {
+    return this.jsonData!.spec;
+  }
+}
+
+export default StatefulSet;
